f02a2b995a6f84df01571d3f905e9e9efebe4f1c
juanmanuel.bouvier <juanmanuel.bouvier@almundo.com>
2018-12-24 19:09:52 -0300

    fix: CBTE txt fields

---
+++ README.md
@@ -0,0 +1,15 @@
+# Parser AFIP
+
+## Instrucciones de ejecución
+
+Para ejecutar el script se debe tener Python 2.7 instalado.
+
+Parámetros de ejecución:
+- Directorio de datos de input (default: `data`)
+- Directorio donde se guardarán los resultados del script (default: `resultados`)
+
+Ejemplo de ejecución:
+```bash
+python compras-parser.py data resultados
+```
+
--- compras-parser.py
+++ compras-parser.py
@@ -9,12 +9,14 @@ FECHA_COMPRA = "FechaCompra"
 TIPO_COMPRA = "TipoCompra"
 PUNTO_VENTA = "PuntoVenta"
 NRO_COMPROBANTE = "NroComprobante"
-DESPACHO_IMPORTACION = "DespachoImprentas"
 ID_VENDEDOR = "IdVendedor"
 RAZON_SOCIAL = "RazonSocial"
 IMP_TOTAL_OP = "ImporteTotalOperacion"
 IMP_NETO_GRAV = "ImporteNetoGravado"
-IMPUESTO_LIQ = "ImpuestoLiquidado"
+IMP_EX_INT_OT = "ImporteConceptosExIntOt"
+IMP_RS_RNI = "ImporteOperacionesExentas"
+IMP_PERCEPCIONES = "ImportePercepciones"
+IVA = "IVA"
 
 
 def load_dataframe(directory, filename):
@@ -137,8 +139,56 @@ def is_neto_gravado(feature, value):
         return False
 
 
+def extract_ex_int_ot(raw):
+    split = raw.split(" ")
+    return split[0]
+
+
+def extract_rs_rni(raw):
+    split = raw.split(" ")
+    if len(split) > 1:
+        rs_rni = split[1]
+    else:
+        rs_rni = split[0]
+    return rs_rni
+
+
+def is_ex_int_ot(feature, value):
+    try:
+        ex_int_ot = extract_ex_int_ot(value)
+        integer, decimal = ex_int_ot.split('.')
+        return "Ex/Int/Ot" in feature
+    except Exception:
+        return False
+
+
+def is_rs_rni(feature, value):
+    try:
+        rs_rni = extract_rs_rni(value)
+        integer, decimal = rs_rni.split('.')
+        return "RS/Rni" in feature
+    except Exception:
+        return False
+
+
 def parse_neto_gravado(raw):
     return parse_decimal_number(extract_neto(raw))
+
+
+def parse_ex_int_ot(raw):
+    return parse_decimal_number(extract_ex_int_ot(raw))
+
+
+def parse_rs_rni(raw):
+    return parse_decimal_number(extract_rs_rni(raw))
+
+
+def is_percepcion(feature, value):
+    try:
+        integer, decimal = value.split('.')
+        return "Percepci" in feature
+    except Exception:
+        return False
 
 
 def is_iva(feature, value):
@@ -157,19 +207,26 @@ def parse(cell, coln, feature, register):
     elif is_nro_comprobante(cell):
         register[PUNTO_VENTA] = parse_pto_venta(cell)
         register[NRO_COMPROBANTE] = parse_nro_comprobante(cell)
-        register[DESPACHO_IMPORTACION] = parse_nro_comprobante(cell)
         if is_neto_gravado(feature, cell):  # Tabula puts them in same column
             register[IMP_NETO_GRAV] = parse_neto_gravado(cell)
+    elif is_neto_gravado(feature, cell):
+        register[IMP_NETO_GRAV] = parse_neto_gravado(cell)
+    elif is_ex_int_ot(feature, cell):
+        register[IMP_EX_INT_OT] = parse_ex_int_ot(cell)
+        if is_rs_rni(feature, cell):        # Tabula puts them in same column
+            register[IMP_RS_RNI] = parse_rs_rni(cell)
+    elif is_rs_rni(feature, cell):
+        register[IMP_RS_RNI] = parse_rs_rni(cell)
+    elif is_percepcion(feature, cell):
+        register[IMP_PERCEPCIONES] = parse_decimal_number(cell)
     elif is_cuit(cell):
         register[ID_VENDEDOR] = parse_id_vendedor(cell)
     elif is_razon_social(coln):
         register[RAZON_SOCIAL] = parse_razon_social(cell)
     elif is_total(feature, cell):
         register[IMP_TOTAL_OP] = parse_decimal_number(cell)
-    elif is_neto_gravado(feature, cell):
-        register[IMP_NETO_GRAV] = parse_neto_gravado(cell)
     elif is_iva(feature, cell):
-        register[IMPUESTO_LIQ] = parse_decimal_number(cell)
+        register[IVA] = parse_decimal_number(cell)
 
 
 def is_valid_register(register):
@@ -177,12 +234,14 @@ def is_valid_register(register):
            and register.has_key(TIPO_COMPRA)\
            and register.has_key(PUNTO_VENTA)\
            and register.has_key(NRO_COMPROBANTE)\
-           and register.has_key(DESPACHO_IMPORTACION)\
            and register.has_key(ID_VENDEDOR)\
            and register.has_key(RAZON_SOCIAL)\
            and register.has_key(IMP_TOTAL_OP)\
            and register.has_key(IMP_NETO_GRAV)\
-           and register.has_key(IMPUESTO_LIQ)
+           and register.has_key(IMP_EX_INT_OT)\
+           and register.has_key(IMP_RS_RNI)\
+           and register.has_key(IMP_PERCEPCIONES)\
+           and register.has_key(IVA)
 
 
 def print_cbte_output(register, output_file):
@@ -190,13 +249,27 @@ def print_cbte_output(register, output_file):
     output_file.write("{}".format(register[TIPO_COMPRA]))
     output_file.write("{}".format(register[PUNTO_VENTA]))
     output_file.write("{}".format(register[NRO_COMPROBANTE]))
-    output_file.write("{}".format(register[DESPACHO_IMPORTACION]))
-    output_file.write("80")     # Codigo de documento del vendedor
+    output_file.write(" " * 16)
+    output_file.write("80")         # Codigo de documento del vendedor
     output_file.write("{}".format(register[ID_VENDEDOR]))
     output_file.write("{}".format(register[RAZON_SOCIAL]))
     output_file.write("{}".format(register[IMP_TOTAL_OP]))
-    output_file.write("000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000PES000100000010000000000000000")
-    output_file.write("{}".format(register[FECHA_COMPRA]))
+    output_file.write("{}".format(register[IMP_EX_INT_OT]))
+    output_file.write("{}".format(register[IMP_RS_RNI]))
+    output_file.write("{}".format(register[IMP_PERCEPCIONES]))
+    output_file.write("0" * 15)     # Percepciones o pagos a cuenta de otros impuestos nacionales
+    output_file.write("0" * 15)     # Percepciones de IIBB
+    output_file.write("0" * 15)     # Percepciones de Impuestos Municipales
+    output_file.write("0" * 15)     # Impuestos Internos
+    output_file.write("PES")        # Codigo moneda
+    output_file.write("0001000000") # Tipo de cambio
+    output_file.write("1")          # Cant alicuotas de IVA
+    output_file.write("5")          # Codigo de operacion
+    output_file.write("{}".format(register[IVA]))
+    output_file.write("0" * 15)     # Otros Tributos
+    output_file.write("0" * 11)     # CUIT emisor/corredor
+    output_file.write(" " * 30)     # Denominacion del emisor/corredor
+    output_file.write("0" * 15)     # IVA comision
     output_file.write(os.linesep)
 
 
@@ -204,11 +277,11 @@ def print_alicuotas_output(register, output_file):
     output_file.write("{}".format(register[TIPO_COMPRA]))
     output_file.write("{}".format(register[PUNTO_VENTA]))
     output_file.write("{}".format(register[NRO_COMPROBANTE]))
-    output_file.write("80")     # Codigo de documento del vendedor
+    output_file.write("80")         # Codigo de documento del vendedor
     output_file.write("{}".format(register[ID_VENDEDOR]))
     output_file.write("{}".format(register[IMP_NETO_GRAV]))
-    output_file.write("0005")   # Alicuota de IVA (21%)
-    output_file.write("{}".format(register[IMPUESTO_LIQ]))
+    output_file.write("0005")       # Alicuota de IVA (21%)
+    output_file.write("{}".format(register[IVA]))
     output_file.write(os.linesep)
 
 
